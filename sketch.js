2ddf76083d3b27dcdba80ece37b22e09b4c34e33
stefania sanchez <stefani2019sanchez@gmail.com>
2024-10-16 21:35:35 -0500

    creando cositas magicas

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255, 182, 193);
+}
+function draw() {
+let pastelBlue=color(173,216,230)
+fill (pastelBlue)
+for(let x=0;x<=width;x+=50)
+    for(let y=0;y<height; y+=50)
+let size=()
+
+
+}
